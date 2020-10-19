36dca6f3d55b31cecf904e08800e153b54ad89f7
Glitch (phrygian-persistent-glockenspiel) <none>
2020-10-19 18:56:18 +0000

    🐴⛱ Checkpoint ./server.js:23967857/1953

--- server.js
+++ server.js
@@ -7,28 +7,28 @@ const bcrypt      = require('bcrypt');
 
 fccTesting(app); //For FCC testing purposes
 
-const saltRounds = 13;
+const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-bcrypt.hash('myPlaintextPassword!', saltRounds, (err, hash) => {
-  console.log(hash);
-  //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
-  bcrypt.compare('myPlaintextPassword!', hash, (err, res) => {
-    console.log(res) //true
+
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash); 
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+      console.log(res); 
   });
 });
-
 
 //END_ASYNC
 
 //START_SYNC
-var hash = bcrypt.hashsync(myPlaintextPassword, saltRounds);
-var result = bcrypt.compareSync(myPlaintextPassword, hash);
-console.log(result);
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash); 
 
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result); 
 
 //END_SYNC
 
