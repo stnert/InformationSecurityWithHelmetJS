72306fabca9611bc94faa7e4e3d63aa3daffd581
Glitch (phrygian-persistent-glockenspiel) <none>
2020-10-19 18:33:12 +0000

    👲📷 Checkpoint ./server.js:23967857/22

--- server.js
+++ server.js
@@ -7,17 +7,17 @@ const bcrypt      = require('bcrypt');
 
 fccTesting(app); //For FCC testing purposes
 
-const saltRounds = 12;
+const saltRounds = 13;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-bcrypt.hash('myPlaintextPassword!', saltRounds, (err, hash) => {
+bcrypt.hash('myPlaintextPassword!', 13, (err, hash) => {
   console.log(hash);
   //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
   bcrypt.compare('myPlaintextPassword!', hash, (err, res) => {
-    console.log(res); //true
+    console.log(res)
   });
 });
 
