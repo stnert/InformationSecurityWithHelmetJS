9f77b1d0d36688a0da72da67c108155a022be976
Glitch (phrygian-persistent-glockenspiel) <none>
2020-10-19 18:09:39 +0000

    🍤🍬 Checkpoint ./server.js:23967857/343 ./package.json:23967857/1630

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
+const bcrypt      = require('bcrypt');
 
 fccTesting(app); //For FCC testing purposes
 
@@ -12,7 +13,13 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
+bcrypt.hash('myPlaintextPassword!', saltRounds, (err, hash) => {
+  console.log(hash);
+  //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+  bcrypt.compare('myPlaintextPassword!', hash, (err, res) => {
+    console.log(res); //true
+  });
+});
 
 
 //END_ASYNC
